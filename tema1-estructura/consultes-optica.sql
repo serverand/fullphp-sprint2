1931dde02f8e1cdb791dfe420c53b51ffbf7364b
Lluis <serverandmail@gmail.com>
2022-10-14 11:49:33 +0200

    tema 1 - estructura v1.1

--- tema1-estructura/consultes-optica.sql
+++ tema1-estructura/consultes-optica.sql
@@ -1,18 +1,4 @@
-SELECT P.nom AS proveidor, MU.nom AS marca, U.preu  
-FROM ulleres AS U
-JOIN clients_ulleres AS CU ON CU.ullera_id = U.id
-JOIN clients AS C ON C.id = CU.client_id
-JOIN proveidor AS P ON P.id = U.proveidor_id
-JOIN marca_ulleres AS MU ON U.marca_id
-WHERE C.id=1 AND U.dia_compra BETWEEN '2021-05-01' AND '2022-05-01'
-GROUP BY U.id
-
-
-
-UPDATE ulleres SET tipus_muntura='metàl·lica' WHERE RAND() < 0.5
-
-
-
+use optica;
 /* *************** primera ******************** */
 SELECT *  
 FROM ulleres AS U
--- tema1-estructura/consultes-pizzaria.sql
+++ tema1-estructura/consultes-pizzaria.sql
@@ -1,3 +1,5 @@
+use pizzeria;
+/* *************** primera ******************** */
 SELECT P.nom AS beguda, CL.localitat FROM productes AS P
 JOIN comandes_productes AS CP ON CP.producte_id = P.id
 JOIN comandes AS C ON C.id = CP.comanda_id
@@ -5,6 +7,8 @@ JOIN client AS CL ON CL.id = C.client_id
 WHERE P.tipus = 'beguda' AND CL.localitat = 'Tarragona'
 GROUP BY CL.id
 
+
+/* *************** segona ******************** */
 SELECT E.*, COUNT(*) AS total_comandes FROM comandes AS C
 JOIN empleat AS E ON E.id = C.repartidor
 WHERE E.id = 3
